d6cfe43a2e1c56bdffd8f97c5e595f4caa85c118
Facundo Zaldo <fzaldo@gmail.com>
2016-05-04 21:27:16 -0300

    Instalacion de humanize duration

--- app/app.js
+++ app/app.js
@@ -1,4 +1,12 @@
 var client = new WebTorrent()
+var humanTime = require('humanize-duration')
+
+var esHumanTime = humanTime.humanizer({
+	language: 'es',
+	largest: 1,
+	round: true
+})
+
 
 var torrentId = 'magnet:?xt=urn:btih:6a9759bffd5c0af65319979fb7832189f4f3c35d&dn=sintel.mp4&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=wss%3A%2F%2Ftracker.webtorrent.io&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel-1024-surround.mp4'
 
@@ -13,7 +21,7 @@ client.add(torrentId, function (torrent) {
 		(torrent.progress * 100).toFixed(1) + '%',
 		torrent.downloadSpeed + " / " + torrent.uploadSpeed,
 		torrent.numPeers,
-		torrent.timeRemaining
+		esHumanTime(torrent.timeRemaining)
 	]).draw(false);
 
 	/*$('#nombre').html(torrent.name)
@@ -28,18 +36,17 @@ client.add(torrentId, function (torrent) {
 
 	
 	torrent.on('download', function (chunkSize) {
-		// Emitted every time a new chunk of data arrives, it's useful for reporting the current torrent status:
 		//currentCalls = t.rows().data();
 		
 		tabla.row(0).data([
-				'1',
-				torrent.name,
-				torrent.downloaded,
-				(torrent.progress * 100).toFixed(1) + '%',
-				torrent.downloadSpeed + " / " + torrent.uploadSpeed,
-				torrent.numPeers,
-				torrent.timeRemaining
-			]).draw();
+			'1',
+			torrent.name,
+			torrent.downloaded,
+			(torrent.progress * 100).toFixed(1) + '%',
+			torrent.downloadSpeed + " / " + torrent.uploadSpeed,
+			torrent.numPeers,
+			esHumanTime(torrent.timeRemaining)
+		]).draw();
 
 
 	})
