5d77c74227e5e4500661235e95750b687a55170f
Facundo Zaldo <fzaldo@gmail.com>
2016-05-04 23:16:52 -0300

    Arreglo de humanize y cambio en el delay de actualizacion

--- app/app.js
+++ app/app.js
@@ -1,12 +1,10 @@
 var client = new WebTorrent()
-var humanTime = require('humanize-duration')
 
-var esHumanTime = humanTime.humanizer({
+var esHumanTime = humanizeDuration.humanizer({
 	language: 'es',
 	largest: 1,
 	round: true
 })
-
 
 var torrentId = 'magnet:?xt=urn:btih:6a9759bffd5c0af65319979fb7832189f4f3c35d&dn=sintel.mp4&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=wss%3A%2F%2Ftracker.webtorrent.io&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel-1024-surround.mp4'
 
@@ -17,9 +15,9 @@ client.add(torrentId, function (torrent) {
 	var fila = tabla.row.add([
 		'1',
 		torrent.name,
-		torrent.downloaded,
+		Humanize.fileSize(torrent.downloaded),
 		(torrent.progress * 100).toFixed(1) + '%',
-		torrent.downloadSpeed + " / " + torrent.uploadSpeed,
+		Humanize.fileSize(torrent.downloadSpeed) + " / " + Humanize.fileSize(torrent.uploadSpeed),
 		torrent.numPeers,
 		esHumanTime(torrent.timeRemaining)
 	]).draw(false);
@@ -34,21 +32,25 @@ client.add(torrentId, function (torrent) {
 */
 
 
-	
-	torrent.on('download', function (chunkSize) {
+//	torrent.on('download', function (chunkSize) {
 		//currentCalls = t.rows().data();
-		
-		tabla.row(0).data([
-			'1',
-			torrent.name,
-			torrent.downloaded,
-			(torrent.progress * 100).toFixed(1) + '%',
-			torrent.downloadSpeed + " / " + torrent.uploadSpeed,
-			torrent.numPeers,
-			esHumanTime(torrent.timeRemaining)
-		]).draw();
+
+        var interval = setInterval(function () {
+			
+			tabla.row(0).data([
+				'1',
+				torrent.name,
+				Humanize.fileSize(torrent.downloaded),
+				(torrent.progress * 100).toFixed(1) + '%',
+				Humanize.fileSize(torrent.downloadSpeed) + " / " + Humanize.fileSize(torrent.uploadSpeed),
+				torrent.numPeers,
+				esHumanTime(torrent.timeRemaining)
+			]).draw();
+
+        }, 500)
 
 
-	})
+
+//	})
 	
 })
