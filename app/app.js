d14bd4ca5ca146bb09eebef5546ea11b48a1841a
Facundo Zaldo <fzaldo@gmail.com>
2016-05-10 22:16:45 -0300

    Manejo de varios torrents al mismo tiempo

--- app/app.js
+++ app/app.js
@@ -1,20 +1,40 @@
+var remote = require('remote')
+var dialog = remote.require('dialog')
+
 var WebTorrent = require('webtorrent')
 var humanizeDuration = require('humanize-duration')
 var Humanize = require('humanize-plus')
-var remote = require('remote'); 
-var dialog = remote.require('dialog'); 
 
 var client = new WebTorrent()
+console.log(client.torrents)
 
-var esHumanTime = humanizeDuration.humanizer({
-	language: 'es',
-	largest: 1,
-	round: true
+var esHumanTime = humanizeDuration.humanizer({ language: 'es', largest: 1, round: true })
+
+var tabla = $('#tabla').DataTable({
+	"paging": false,
+	"lengthChange": false,
+	"searching": false,
+	"ordering": true,
+	"info": true,
+	"autoWidth": true,
+	"bInfo": false,
+	"columns": [
+			{ title: "#" },
+			{ title: "Nombre" },
+			{ title: "Tamaño" },
+			{ title: "Progreso" },
+			{ title: "Velocidad" },
+			{ title: "Seeds/Peers" },
+			{ title: "Tiempo Est." }
+		]
 })
+
+/*** DEBUG ***/
 var torrentId = 'magnet:?xt=urn:btih:6a9759bffd5c0af65319979fb7832189f4f3c35d&dn=sintel.mp4&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=wss%3A%2F%2Ftracker.webtorrent.io&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel-1024-surround.mp4'
-var torrentId = "magnet:?xt=urn:btih:DF19459B13F9E6B57347DBEE54F2DBBD751B914A&dn=10+cloverfield+lane+2016+1080p+hdrip+x264+aac+jyk&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80%2Fannounce&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce"
-var torrentId = "/home/facuz/Descargas/sintel.torrent"
-$('#tb-agregar-file').val(torrentId) // PARA TESTEO NADA MAS
+var torrentId1 = "magnet:?xt=urn:btih:DF19459B13F9E6B57347DBEE54F2DBBD751B914A&dn=10+cloverfield+lane+2016+1080p+hdrip+x264+aac+jyk&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80%2Fannounce&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce"
+var torrentId2 = "/home/facuz/Descargas/sintel.torrent"
+$('#tb-agregar-file').val(torrentId)
+/*** END DEBUG ***/
 
 
 /*** Agregar Torrent Dialog ***/
@@ -32,14 +52,6 @@ $('#btn-agregar-fileDialog').click(function (){
 		// 	document.getElementById("editor").value = data;*/
 		// });
 	})
-
-	
-	// dialog.showOpenDialog({ properties: [ 'openFile', 'openDirectory']})
-	
-	// dialog.setContext(document) 
-	// dialog.openFileDialog('.torrent', '/', function(result) {
-	//     $('#tb-agregar-file').val(result)
-	// })
 })
 
 $('#btn-agregar-folderDialog').click(function (){
@@ -48,36 +60,45 @@ $('#btn-agregar-folderDialog').click(function (){
 	dialog.showOpenDialog(filtros, function (folderName) {
 		$('#tb-agregar-folder').val(folderName)
 	})
-
 })
 
 $('#btn-agregar-download').click(function (){
+	addTorrent($('#tb-agregar-file').val())
 
-	console.log($('#tb-agregar-file').val());
+	//return e.preventDefault()
+})
 
-	client.add($('#tb-agregar-file').val(), function (torrent) {
-		console.log(torrent);
+
+function addTorrent(torrentID){
+	var fila = tabla.row.add([
+		'#',
+		'Cargando...',
+		'0 Kb/s',
+		'0%',
+		'0 Kb/s | 0 Kb/s',
+		'0',
+		'-'
+	]).draw()
+
+	client.add(torrentID, function (torrent) {
 		//var file = torrent.files[0]
-		//console.log(tabla);
-		
-		var fila = tabla.row.add([
-			'1',
-			torrent.name,
-			Humanize.fileSize(torrent.downloaded),
-			(torrent.progress * 100).toFixed(1) + '%',
-			Humanize.fileSize(torrent.downloadSpeed) + " / " + Humanize.fileSize(torrent.uploadSpeed),
-			torrent.numPeers,
-			esHumanTime(torrent.timeRemaining)
-		]).draw(false)
 
 		torrent.on('error', function (err) {
 			console.log("salto un error")
 		})
+		
+	})
 
-		var interval = setInterval(function () {
-			
-			tabla.row(0).data([
-				'1',
+}
+
+
+var interval = setInterval(function () {
+	let count = client.torrents.length 
+	if(count > 0){
+		for (var i = count - 1; i >= 0; i--) {
+			torrent = client.torrents[i]
+			tabla.row(i).data([
+				i,
 				torrent.name,
 				Humanize.fileSize(torrent.downloaded),
 				(torrent.progress * 100).toFixed(1) + '%',
@@ -85,12 +106,10 @@ $('#btn-agregar-download').click(function (){
 				torrent.numPeers,
 				esHumanTime(torrent.timeRemaining)
 			]).draw()
+		}
+	}
 
-		}, 500)
-		
-	})
+}, 500)
 
-
-	//return e.preventDefault()
-
-})
+addTorrent(torrentId)
+addTorrent(torrentId1)
