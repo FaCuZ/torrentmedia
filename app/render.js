7f31be20c2b364cd7fb1593c25b499b14a938828
Facundo Zaldo <fzaldo@gmail.com>
2016-06-07 12:26:48 -0300

    Arreglos menores y emprolijado de codigo

--- app/render.js
+++ app/render.js
@@ -7,18 +7,39 @@ const Electron 		= require('electron'),
 	  WebTorrent 	= require('webtorrent'),
 	  Humanize 		= require('humanize-plus')
 
-var client 		= new WebTorrent(),
-	settings 	= remote.getGlobal('settings'),  
-	locale 		= require('./json/locale/' + settings.locale + '.json'),
-	humanTime 	= require('humanize-duration').humanizer({ language: settings.locale, largest: 1, round: true }),
-	table 		= $('#table').DataTable(tableConfig()),
-	footer 		= $(".main-footer").html(generalFoot()),
-	torrents 	= initTorrents()
+var client		= new WebTorrent(),
+	settings	= remote.getGlobal('settings'),  
+	locale		= require('./json/locale/' + settings.locale + '.json'),
+	humanTime	= require('humanize-duration').humanizer({ language: settings.locale, largest: 1, round: true }),
+	table		= $('#table').DataTable(tableConfig()),
+	footer		= $(".main-footer").html(generalFoot())
 
 
 ////////////////////
 ////-- EVENTS --////
 ////////////////////
+var call = {
+		btn_add_modal 		 :()=> gui.addTorrentModal(),
+		btn_add_download 	 :()=> addTorrent($('#tb-add-file').val()),
+		btn_remove	 		 :()=> removeTorrent(table.row('tr.selected')),
+		btn_pause			 :()=> pauseTorrent(table.row('tr.selected')),	
+		btn_position_up 	 :()=> torrents[torrentSelected.infoHash].up(),
+		btn_position_down 	 :()=> torrents[torrentSelected.infoHash].down(),
+		btn_nav_main 		 :()=> gui.changePage('main'),
+		btn_nav_stats 		 :()=> gui.changePage('stats'),
+		btn_nav_autofeeds 	 :()=> gui.changePage('autofeeds'),
+		btn_nav_channels 	 :()=> gui.changePage('channels'),
+		btn_add_fileDialog	 :()=> gui.getDialogFile(), 
+		btn_add_folderDialog :()=> gui.getDialogFolder(),
+		btn_search	 		 :()=> gui.searchTorrent(),
+		btn_bottom_settings	 :()=> gui.changePage('settings'),
+		btn_bottom_fullscreen:()=> ipcRenderer.send('control', 'fullscreen'),
+		btn_bottom_private	 :()=> alert('private'),
+		btn_bottom_hide		 :()=> ipcRenderer.send('control', 'hide'),
+		btn_bottom_close	 :()=> ipcRenderer.send('control', 'close'),
+		btn_sidebar_toggle	 :()=> $('.sidebar-footer').toggle()
+	}
+
 $('#table tbody').on( 'click', 'tr', function () {
 	if($('#table tbody td').hasClass('dataTables_empty')) return false 
 
@@ -34,45 +55,23 @@ $('#table tbody').on( 'click', 'tr', function () {
 	}
 })
 
-var call = {
-		btn_add_download 	 :()=> addTorrent($('#tb-agregar-file').val()),
-		btn_remove	 		 :()=> removeTorrent(table.row('tr.selected')),
-		btn_pause			 :()=> pauseTorrent(table.row('tr.selected')),	
-		btn_position_up 	 :()=> torrents[torrentSelected.infoHash].up(),
-		btn_position_down 	 :()=> torrents[torrentSelected.infoHash].down(),
-		btn_nav_main 		 :()=> changeContent('main'),
-		btn_nav_stats 		 :()=> changeContent('stats'),
-		btn_nav_autofeeds 	 :()=> changeContent('autofeeds'),
-		btn_nav_channels 	 :()=> changeContent('channels'),
-		btn_add_fileDialog	 :()=> getDialog(), 
-		btn_add_folderDialog :()=> getDialog(true),
-		btn_search	 		 :()=> searchTorrent(),
-		btn_bottom_settings	 :()=> changeContent('settings'),
-		btn_bottom_fullscreen:()=> ipcRenderer.send('control', 'fullscreen'),
-		btn_bottom_private	 :()=> alert('private'),
-		btn_bottom_hide		 :()=> ipcRenderer.send('control', 'hide'),
-		btn_bottom_close	 :()=> ipcRenderer.send('control', 'close'),
-		btn_sidebar_toggle	 :()=> $('.sidebar-footer').toggle()
-}
-
-
 ///////////////////////
 ////-- FUNCTIONS --////
 ///////////////////////
-function initTorrents(){
-	return {
-			find: position => {
-				for (var i in torrents) 
-					if(torrents[i].position === position)
-						return torrents[i]
-				},
-			length: () => {
-				let length = -2
-				for (var i in torrents)	length++
-				return length
-				}
-			}
+var	torrents = {
+	find: position => {
+		for (var i in torrents) 
+			if(torrents[i].position === position)
+				return torrents[i]
+	},
+	
+	length: () => {
+		let length = -2
+		for (var i in torrents)	length++
+		return length
+	}
 }
+
 
 function tableConfig(){
 	let json = require('./json/datatable.json')
@@ -84,36 +83,49 @@ function tableConfig(){
 
 }
 
-function changeContent(type){
-	$('.content-wrapper').hide()
-	$('.content-wrapper-' + type).show()
-	$('.sidebar-menu li').removeClass('active')
-	$('#btn_nav_' + type).addClass('active')	
-}
+var gui = {
+	addTorrentModal: () => {		
+		$('#addModal').modal('toggle')
+		$('#tb-add-folder').val(settings.dir_downloads)
+	},
 
-function searchTorrent(){
-	shell.openExternal(settings.searchers[settings.search_with] + $('#tb-search').val())
-}
+	changePage: type => {
+		$('.content-wrapper').hide()
+		$('.content-wrapper-' + type).show()
+		$('.sidebar-menu li').removeClass('active')
+		$('#btn_nav_' + type).addClass('active')	
+	},
 
-function getDialog(folder = false){
-	let config, tb
-	if(!folder) {
-		config = {	title: 'Please select a torrent',
-					filters: [{ name: 'Torrents', extensions: ['torrent'] }],
-					defaultPath: settings.dir_downloads,
-					properties: ['openFile']
-				 }
-		tb = $('#tb-agregar-file')
-	} else {
-		config = {  title: 'Please select a folder',
-					defaultPath: settings.dir_downloads,
-					properties: ['openDirectory', 'createDirectory']
-				 }		
-		tb = $('#tb-agregar-folder')
+	searchTorrent: () => {
+		shell.openExternal(settings.searchers[settings.search_with] + $('#tb-search').val())
+	},
+
+	getDialogFile: () => {
+		let config = {	title: 'Please select a torrent',
+						filters: [{ name: 'Torrents', extensions: ['torrent'] }],
+						defaultPath: settings.dir_downloads,
+						properties: ['openFile']
+					 }
+
+		dialog.showOpenDialog(config, name => $('#tb-add-file').val(name))	
+	},
+
+	getDialogFolder: () => {
+		let config = {  title: 'Please select a folder',
+						defaultPath: settings.dir_downloads,
+						properties: ['openDirectory', 'createDirectory']
+					 }		
+
+		dialog.showOpenDialog(config, name => $('#tb-add-folder').val(name))	
+	},
+
+	setTray: (blink, color) => {
+		settings.tray_blink = blink
+		settings.tray_color = color
 	}
 
-	dialog.showOpenDialog(config, name => tb.val(name))	
 }
+
 
 function addTorrent(torrentID){
 	let temp = table.row.add([
@@ -128,7 +140,7 @@ function addTorrent(torrentID){
 		'-'
 	]).draw()
 	
-	let torrent = client.add(torrentID, torrent => {  })
+	let torrent = client.add(torrentID,	{ path: $('#tb-add-folder').val() }, torrent => {  })
 
 	torrent.on('infoHash', error => {
 		$(temp.node()).attr('data-hash', torrent.infoHash)
@@ -137,19 +149,14 @@ function addTorrent(torrentID){
 
 	torrent.on('error', error => {
 		temp.row().remove().draw()
-		setTray(false, 'red')
+		gui.setTray(false, 'red')
 		alert(error)
 		//TODO: cambiar el alert por:
 		//dialog.showErrorBox(title, content)
 	})
 
-	torrent.on('done', () => setTray(true, 'green'))
+	torrent.on('done', () => gui.setTray(true, 'green'))
 
-}
-
-function setTray(blink, color){
-	settings.tray_blink = blink
-	settings.tray_color = color
 }
 
 function removeTorrent(row){
@@ -174,7 +181,7 @@ function pauseTorrent(row){
 
 function fixRatio(ratio){
 	if(ratio > 1000) return "&infin;"
-	return Humanize.formatNumber(ratio, 2)	
+	return Humanize.formatNumber(ratio, 2)
 }
 
 function progressBar(progress, element, torrent = null){ 
@@ -222,6 +229,48 @@ function generalFoot (){
 ///////////////////////
 ////-- INTERVALS --////
 ///////////////////////
+var interval = {
+	table: null,
+	tray: null,
+	updateTable: () => {
+		let count = client.torrents.length
+		
+		if(count <= 0) return null
+
+		for (var i = count - 1; i >= 0; i--) {
+			torrent = client.torrents[i]
+			table.row(i).data([
+				torrents[torrent.infoHash].position,
+				torrent.name,
+				Humanize.fileSize(torrent.downloaded),
+				progressBar((torrent.progress * 100).toFixed(1), $('#table'), torrent),
+				Humanize.fileSize(torrent.downloadSpeed) + "/s",
+				Humanize.fileSize(torrent.uploadSpeed) + "/s",
+				torrent.numPeers,
+				fixRatio(torrent.ratio),
+				humanTime(torrent.timeRemaining)
+			]).draw()
+		}
+
+		$(".main-footer").html(generalFoot())
+	},
+
+	updateTray: () => {
+		let ds = Humanize.fileSize(client.downloadSpeed) + '/s'
+		let us = Humanize.fileSize(client.uploadSpeed) + '/s'
+
+		ipcRenderer.send('tray', ' ↓ ' + ds + ' ↑ ' + us, settings.tray_blink, settings.tray_color)
+	},
+
+	startAll: () => {	
+		interval.table = setInterval(() => interval.updateTable(), settings.interval_refresh)
+		interval.tray = setInterval(() => interval.updateTray(), settings.interval_tray)
+	}
+
+}
+
+interval.startAll()
+/*
 var interval = setInterval( () => {
 	let count = client.torrents.length
 	
@@ -253,7 +302,7 @@ var intervalTray = setInterval( () => {
 	ipcRenderer.send('tray', ' ↓ ' + ds + ' ↑ ' + us, settings.tray_blink, settings.tray_color)
 
 }, settings.interval_tray)
-
+*/
 
 /////////////////////
 ////-- CLASSES --////
@@ -291,11 +340,10 @@ class Torrent {
 ////** DEBUG **////
 ///////////////////
 var tId0 = 'magnet:?xt=urn:btih:6a9759bffd5c0af65319979fb7832189f4f3c35d&dn=sintel.mp4&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=wss%3A%2F%2Ftracker.webtorrent.io&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel-1024-surround.mp4'
-var tId1 = "magnet:?xt=urn:btih:DF19459B13F9E6B57347DBEE54F2DBBD751B914A&dn=10+cloverfield+lane+2016+1080p+hdrip+x264+aac+jyk&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80%2Fannounce&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce"
 var tId2 = "/home/facuz/Descargas/sintel.torrent"
 
-$('#tb-agregar-file').val(tId0)
+$('#tb-add-file').val(tId0)
 
-addTorrent(tId0)
-addTorrent(tId1)
+//addTorrent(tId0)
+//addTorrent(tId1)
 ////** END DEBUG **////
--- app/tpl/pages/main.tpl
+++ app/tpl/pages/main.tpl
@@ -11,7 +11,6 @@
 			
 			<span id="btns-hided" class="hide-btns">
 				<button type="button" onclick="call.btn_pause()" class="btn btn-default" id="btn-pause"><i class="fa fa-pause"></i></button>
-				<!-- TODO: Cambio a fa-play -->
 
 				<div class="btn-group">
 					<button type="button" onclick="call.btn_position_up()" class="btn btn-default"><i class="fa fa-arrow-up"></i></button>
@@ -21,18 +20,18 @@
 				<button type="button" onclick="call.btn_remove()" class="btn btn-default"><i class="fa fa-remove"></i></button>
 			</span>
 
-			<button type="button" class="btn btn-default" data-toggle="modal" data-target="#myModal"><i class="fa fa-plus"></i></button>
+			<button type="button" class="btn btn-default" data-toggle="----tooltip" data-original-title="{{ tooltip.add_modal }}" data-placement="bottom" onclick="call.btn_add_modal()"><i class="fa fa-plus"></i></button>
 		</div>
 
 
 		<!-- MODAL: Agregar Torrent -->
-		<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+		<div class="modal fade" id="addModal" tabindex="-1" role="dialog" aria-labelledby="addModalLabel">
 			<div class="modal-dialog" role="document">
 				<div class="modal-content">
 
 					<div class="modal-header">
 						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title" id="myModalLabel">{{ downloads.new_torrent }}</h4>
+						<h4 class="modal-title" id="addModalLabel">{{ downloads.new_torrent }}</h4>
 					</div>
 
 					<div class="modal-body">
@@ -40,7 +39,7 @@
 							<div class="form-group">
 								<label for="torrent-tb" class="control-label">{{ downloads.download_torrent}}</label>
 								<div class="input-group">
-									<input type="text" class="form-control" id="tb-agregar-file" placeholder="{{ downloads.download_torrent_ph }}" >
+									<input type="text" class="form-control" id="tb-add-file" placeholder="{{ downloads.download_torrent_ph }}" >
 									<div class="input-group-btn">
 										<button type="button" class="btn btn-default" onclick="call.btn_add_fileDialog()">
 											<i class="fa fa-fw fa-file-o"></i>
@@ -52,7 +51,7 @@
 							<div class="form-group">
 								<label for="ubicacion-tb" class="control-label">{{ downloads.download_folder }}</label>
 								<div class="input-group">
-									<input type="text" class="form-control" id="tb-agregar-folder">
+									<input type="text" class="form-control" id="tb-add-folder">
 									<div class="input-group-btn">
 										<button type="button" class="btn btn-default" onclick="call.btn_add_folderDialog()">
 											<i class="fa fa-fw fa-folder-open-o"></i>
